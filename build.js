f4f094f990661c7fd4cd5adfa2ee726118ea88e9
Will Myers <griffin.myers@gmail.com>
2015-01-24 16:38:53 -0500

    Added stuff

--- build.js
+++ build.js
@@ -1,22 +1,41 @@
-var Metalsmith = require('metalsmith');
-var markdown = require('metalsmith-markdown');
-var templates = require('metalsmith-templates');
-
 var _ = require('lodash');
 var path = require('path');
 
+var Metalsmith = require('metalsmith');
+var markdown = require('metalsmith-markdown');
+var templates = require('metalsmith-templates');
+var serve = require('metalsmith-serve');
+var sass = require('metalsmith-sass');
+var watch = require('metalsmith-watch');
+var asset = require('metalsmith-static');
+
+var markdown_options = {
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: true
+};
+
+var sass_options = {
+  outputDir: 'css'
+}
+
+var asset_options = {
+  src: 'images',
+  dest: 'images'
+}
+
 Metalsmith(__dirname)
-  .use(markdown({
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: true,
-    smartLists: true,
-    smartypants: true
-  }))
+  .use(watch())
+  .use(asset(asset_options))
+  .use(markdown(markdown_options))
   .use(bind_template())
   .use(templates('jade'))
+  .use(sass(sass_options))
+  .use(serve())
   .build(build_handler);
 
 function build_handler(error, files) {
