9ffe54de9540a285616f7877e0385c931caab5d0
William Myers <griffin.myers@gmail.com>
2015-02-06 00:13:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var path = require('path');
 global.root_require = function root_require(p) { return require(path.join(__dirname, p)); }
 
-var url = require('url');
-var _ = require('lodash');
 var app = require('express')();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var config = root_require('config');
 var logger = root_require('lib').logger;
 var verify = root_require('lib').verify;
+
 var AuthorizeController = root_require('controllers').AuthorizeController;
 var DeployController = root_require('controllers').DeployController;
 
