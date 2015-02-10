00e21bf2bb8540f114d3ae908fcc9dd4ae68b7f2
Will Myers <griffin.myers@gmail.com>
2015-02-10 00:31:59 -0500

    Refactored build

--- config/default.js
+++ config/default.js
@@ -1,8 +1,11 @@
+var path = require('path');
+
 module.exports = {
   port: process.env.PORT || 3000,
   redirect_uri: 'http://localhost:3000/authorize/redirect',
   destination: 'build',
   source: 'src',
+  root: path.resolve(__dirname, '..'),
   key_bucket: 'dropbox-keys',
   users: require('./users')
 };
