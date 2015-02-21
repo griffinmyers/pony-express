c3f183265f7eb8202ff2b7241e9d3c411d2212f2
Will Myers <griffin.myers@gmail.com>
2015-02-21 15:44:57 -0500

    Made build.js multi-tenant

--- build.js
+++ build.js
@@ -1,9 +1,20 @@
+var path = require('path');
 var config = require('./config');
 var build = require('./lib/build');
 
-var is_dev = process.argv.length > 2 && process.argv[2] === 'dev' || false
+if(process.argv.length < 3) {
+  console.log('Usage: node build.js id [dev]');
+  return;
+}
+
+var id = process.argv[2];
+
+var staging_dir = path.join(config.source, id.toString());
+var build_dir = path.join(config.destination, id.toString());
+
+var is_dev = process.argv.length > 3 && process.argv[3] === 'dev' || false
 var is_script = !module.parent;
 
 if(is_script) {
-  build(config.root, config.source, config.destination, is_dev).done();
+  build(config.root, staging_dir, build_dir, is_dev).done();
 }
