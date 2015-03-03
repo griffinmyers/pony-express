24e3333d02dc8b2562b35195da64e691f69d518a
Will Myers <griffin.myers@gmail.com>
2015-03-03 01:35:42 -0500

    Added some middleware for blagging

--- config/users.js
+++ config/users.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var moment = require('moment');
 
 module.exports = {
   43720278: {
@@ -19,14 +20,37 @@ module.exports = {
       ];
     }
   },
+  // 544017: {
+  //   bucket: 'pony-express-dev',
+  //   middleware: function(source) {
+  //     return [
+  //       {name: 'asset', args: {src: 'images', dest: 'images'}},
+  //       {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
+  //       {name: 'bind_template'},
+  //       {name: 'two_column'},
+  //       {name: 'row'},
+  //       {name: 'partial'},
+  //       {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
+  //       {name: 'sass', args: {outputDir: 'assets/css'}},
+  //       {name: 'uglify', args: {removeOriginal: true}},
+  //       {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
+  //       {name: 'clean', args: '_code'}
+  //     ];
+  //   }
+  // },
   544017: {
-    bucket: 'joeshaner-dev',
+    bucket: 'wgm.cool',
     middleware: function(source) {
       return [
         {name: 'asset', args: {src: 'images', dest: 'images'}},
+        {name: 'collections', args: {posts: {pattern: 'blog/**/*.md', sortBy: 'date', reverse: true, metadata: {template: 'page'}}}},
         {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
+        {name: 'wrap'},
+        {name: 'clean', args: 'blog'},
+        {name: 'page', args: {collection: 'posts', perPage: 3, target: 'blog', template: 'blog'}},
         {name: 'bind_template'},
         {name: 'partial'},
+        {name: 'expose', args: {moment: moment}},
         {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
         {name: 'sass', args: {outputDir: 'assets/css'}},
         {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
