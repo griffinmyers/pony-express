c2f30be7c33b201589c8532b5484c899900d5018
Will Myers <griffin.myers@gmail.com>
2015-03-07 13:33:20 -0500

    Refactoring

--- routes.js
+++ routes.js
@@ -1,16 +1,13 @@
 var router = require('express').Router();
 var bodyParser = require('body-parser');
 var verify = root_require('lib').verify;
+var c = root_require('controllers');
 
-var RootController = root_require('controllers').RootController;
-var AuthorizeController = root_require('controllers').AuthorizeController;
-var DeployController = root_require('controllers').DeployController;
-
-router.get ('/', RootController.index);
-router.get ('/authorize', AuthorizeController.authorize);
-router.get ('/authorize/redirect', AuthorizeController.redirect);
-router.get ('/deploy', DeployController.challenge);
-router.post('/deploy', bodyParser.json({verify: verify}), DeployController.deploy);
-router.post('/deploy/sync', bodyParser.json(), DeployController.deploy_sync);
+router.get ('/', c.RootController.index);
+router.get ('/authorize', c.AuthorizeController.authorize);
+router.get ('/authorize/redirect', c.AuthorizeController.redirect);
+router.get ('/deploy', c.DeployController.challenge);
+router.post('/deploy', bodyParser.json({verify: verify}), c.DeployController.deploy);
+router.post('/deploy/sync', bodyParser.json(), c.DeployController.deploy_sync);
 
 module.exports = router;
