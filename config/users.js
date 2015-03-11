f2f0cc426a4f6a605e17c768e3de6e9bb6b0eb10
Will Myers <griffin.myers@gmail.com>
2015-03-11 00:17:28 -0400

    Refactored middleware + added permalinks

--- config/users.js
+++ config/users.js
@@ -7,7 +7,7 @@ module.exports = {
     middleware: function(source) {
       return [
         {name: 'asset', args: {src: 'images', dest: 'images'}},
-        {name: 'collections', args: {posts: {pattern: 'blog/**/*.md', sortBy: 'date', reverse: true, metadata: {template: 'page'}}}},
+        {name: 'collections', args: {posts: {pattern: 'blog/**/*.md', sortBy: 'date', reverse: true}}},
         {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
         {name: 'wrap'},
         {name: 'clean', args: 'blog'},
@@ -26,46 +26,45 @@ module.exports = {
       ];
     }
   },
-  // 544017: {
-  //   bucket: 'pony-express-dev',
-  //   middleware: function(source) {
-  //     return [
-  //       {name: 'asset', args: {src: 'images', dest: 'images'}},
-  //       {name: 'collections', args: {posts: {pattern: 'blog/**/*.md', sortBy: 'date', reverse: true, metadata: {template: 'page'}}}},
-  //       {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
-  //       {name: 'wrap'},
-  //       {name: 'clean', args: 'blog'},
-  //       {name: 'page', args: {collection: 'posts', perPage: 3, target: 'blog', template: 'blog'}},
-  //       {name: 'bind_template'},
-  //       {name: 'two_column'},
-  //       {name: 'row'},
-  //       {name: 'partial'},
-  //       {name: 'expose', args: {moment: moment}},
-  //       {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
-  //       {name: 'sass', args: {outputDir: 'assets/css'}},
-  //       {name: 'uglify', args: {removeOriginal: true}},
-  //       {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
-  //       {name: 'clean', args: '_code'},
-  //       {name: 'clean', args: '.pony-token'}
-  //     ];
-  //   }
-  // },
   544017: {
     bucket: 'wgm.cool',
     middleware: function(source) {
       return [
         {name: 'asset', args: {src: 'images', dest: 'images'}},
-        {name: 'collections', args: {posts: {pattern: 'blog/**/*.md', sortBy: 'date', reverse: true, metadata: {template: 'page'}}}},
+        {name: 'collections', args: {posts: {pattern: 'blog/**/*.md', sortBy: 'date', reverse: true}}},
         {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
         {name: 'wrap'},
         {name: 'clean', args: 'blog'},
-        {name: 'page', args: {collection: 'posts', perPage: 3, target: 'blog', template: 'blog'}},
+        {name: 'page', args: {collection: 'posts', perPage: 3, target: 'blog', template: 'blog', perma: true}},
         {name: 'bind_template'},
         {name: 'partial'},
         {name: 'expose', args: {moment: moment}},
         {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
         {name: 'sass', args: {outputDir: 'assets/css'}},
         {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
+        {name: 'clean', args: '_code'},
+        {name: 'clean', args: '.pony-token'}
+      ];
+    }
+  },
+  dev: {
+    bucket: 'pony-express-dev',
+    middleware: function(source) {
+      return [
+        {name: 'asset', args: {src: 'images', dest: 'images'}},
+        {name: 'collections', args: {recipes: {pattern: 'recipes/**/*.md', sortBy: 'date', reverse: true}}},
+        {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
+        {name: 'wrap'},
+        {name: 'clean', args: 'recipes'},
+        {name: 'page', args: {collection: 'recipes', perPage: 3, target: 'recipes', template: 'recipes'}},
+        {name: 'bind_template'},
+        {name: 'partial'},
+        {name: 'expose', args: {moment: moment}},
+        {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
+        {name: 'sass', args: {outputDir: 'assets/css'}},
+        {name: 'uglify', args: {removeOriginal: true}},
+        {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
+        {name: 'move', args: {source: '_code/fonts', destination: 'assets/fonts'}},
         {name: 'clean', args: '_code'},
         {name: 'clean', args: '.pony-token'}
       ];
