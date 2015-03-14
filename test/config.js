ea387305fbbd3aa31c1b5ffe272ea6f416704e3a
Will Myers <griffin.myers@gmail.com>
2015-03-14 14:15:13 -0400

    Basic testing framework

---
+++ test/config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var should = require('should');
+var assert = require('assert');
+
+describe('Config', function(){
+
+  describe('default', function() {
+    it('loads the default config', function() {
+      var c = require('../config');
+      c.redirect_uri.should.be.exactly('http://localhost:3000/authorize/redirect');
+    });
+  });
+
+  describe('dev', function() {
+    it('loads the dev config', function() {
+      process.env.NODE_ENV = 'dev';
+      delete require.cache[path.join(__dirname, '..', 'config', 'index.js')]
+      var c = require('../config');
+      c.redirect_uri.should.be.exactly('https://ponyexprss.com/authorize/redirect');
+    });
+  });
+
+});
