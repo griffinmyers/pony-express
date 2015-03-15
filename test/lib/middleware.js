5775347fb8241de4163d1118a8fd117f387f0aa2
Will Myers <griffin.myers@gmail.com>
2015-03-15 18:53:04 -0400

    Added clean test

--- test/lib/middleware.js
+++ test/lib/middleware.js
@@ -1,25 +1,26 @@
 var should = require('should');
 var middleware = require('../../lib/middleware');
 var bind_template = middleware.bind_template;
+var clean = middleware.clean;
 
 describe('Middlware', function() {
 
+  var files;
+
+  beforeEach(function() {
+    files = {
+      'b.html': {template: 'page'},
+      'a/b.html': {template: 'page'},
+      'a/c.html': {},
+      'a/b.css': {}
+    }
+  });
+
   describe('bind_template()', function() {
-
-    var files;
-
-    beforeEach(function() {
-      files = {
-        'a.html': {template: 'page'},
-        'a/b.html': {template: 'page'},
-        'a/c.html': {},
-        'a/b.css': {}
-      }
-    });
 
     it('adds .jade to html', function() {
       bind_template()(files);
-      files['a.html'].should.have.property('template', 'page.jade');
+      files['b.html'].should.have.property('template', 'page.jade');
       files['a/b.html'].should.have.property('template', 'page.jade');
     });
 
@@ -31,6 +32,26 @@ describe('Middlware', function() {
     it('adds leaves non-html files alone', function() {
       bind_template()(files);
       files['a/b.css'].should.be.empty;
+    });
+
+  });
+
+  describe('clean()', function() {
+
+    it('removes files', function() {
+      clean('b.html')(files);
+      files.should.not.have.property('b.html');
+      files.should.have.property('a/b.html');
+      files.should.have.property('a/c.html');
+      files.should.have.property('a/b.css');
+    });
+
+    it('removes files based on the prefix', function() {
+      clean('a')(files);
+      files.should.have.property('b.html');
+      files.should.not.have.property('a/b.html');
+      files.should.not.have.property('a/c.html');
+      files.should.not.have.property('a/b.css');
     });
 
   });
