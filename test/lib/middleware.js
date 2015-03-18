3e73c32d4aaa8b7b9b8c34ee1e084a57bab2577f
Will Myers <griffin.myers@gmail.com>
2015-03-17 22:48:07 -0400

    Adding move test

--- test/lib/middleware.js
+++ test/lib/middleware.js
@@ -2,6 +2,7 @@ var should = require('should');
 var middleware = require('../../lib/middleware');
 var bind_template = middleware.bind_template;
 var clean = middleware.clean;
+var move = middleware.move;
 
 describe('Middlware', function() {
 
@@ -12,7 +13,8 @@ describe('Middlware', function() {
       'b.html': {template: 'page'},
       'a/b.html': {template: 'page'},
       'a/c.html': {},
-      'a/b.css': {}
+      'a/b.css': {},
+      'a/d/e.js': {}
     }
   });
 
@@ -52,6 +54,37 @@ describe('Middlware', function() {
       files.should.not.have.property('a/b.html');
       files.should.not.have.property('a/c.html');
       files.should.not.have.property('a/b.css');
+    });
+
+  });
+
+  describe('move()', function() {
+
+    it('moves files', function() {
+      move({source: 'a', destination: 'c'})(files)
+      files.should.have.property('b.html');
+      files.should.have.property('c/b.html');
+      files.should.have.property('c/c.html');
+      files.should.have.property('c/b.css');
+      files.should.have.property('c/d/e.js');
+      files.should.not.have.property('a/b.html');
+      files.should.not.have.property('a/c.html');
+      files.should.not.have.property('a/b.css');
+    });
+
+    it('moves files from one nested dir to another', function() {
+      move({source: 'a/d', destination: 'a'})(files);
+      files.should.have.property('b.html');
+      files.should.have.property('a/b.html');
+      files.should.have.property('a/c.html');
+      files.should.have.property('a/b.css');
+      files.should.have.property('a/e.js');
+      files.should.not.have.property('a/d/e.js');
+    });
+
+    it('overrides existing files', function() {
+      move({source: 'a/b.html', destination: 'a/c.html'})(files);
+      files.should.containDeep({'a/c.html': {template: 'page'}});
     });
 
   });
