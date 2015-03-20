64bbc08b29071c9013b07c7611e2679969bd44a2
Will Myers <griffin.myers@gmail.com>
2015-03-20 01:26:08 -0400

    Adding fallcreekfarm.org

--- config/users.js
+++ config/users.js
@@ -47,6 +47,29 @@ module.exports = {
       ];
     }
   },
+  59006125: {
+    bucket: 'fallcreekfarm.org',
+    middleware: function(source) {
+      return [
+        {name: 'asset', args: {src: 'images', dest: 'images'}},
+        {name: 'collections', args: {recipes: {pattern: 'recipes/**/*.md', sortBy: 'date', reverse: true}}},
+        {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
+        {name: 'wrap'},
+        {name: 'clean', args: 'recipes'},
+        {name: 'page', args: {collection: 'recipes', perPage: 3, target: 'recipes', template: 'recipes'}},
+        {name: 'bind_template'},
+        {name: 'partial'},
+        {name: 'expose', args: {moment: moment}},
+        {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
+        {name: 'sass', args: {outputDir: 'assets/css'}},
+        {name: 'uglify', args: {removeOriginal: true}},
+        {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
+        {name: 'move', args: {source: '_code/fonts', destination: 'assets/fonts'}},
+        {name: 'clean', args: '_code'},
+        {name: 'clean', args: '.pony-token'}
+      ];
+    }
+  },
   dev: {
     bucket: 'pony-express-dev',
     middleware: function(source) {
