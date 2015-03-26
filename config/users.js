141d8c19884ca227e29a287a7d1f37e1ac4ec3bf
Will Myers <griffin.myers@gmail.com>
2015-03-26 04:41:27 -0400

    Adding fonts asset to Joph

--- config/users.js
+++ config/users.js
@@ -7,6 +7,7 @@ module.exports = {
     middleware: function(source) {
       return [
         {name: 'asset', args: {src: 'images', dest: 'images'}},
+        {name: 'asset', args: {src: 'fonts', dest: 'fonts'}},
         {name: 'collections', args: {posts: {pattern: 'blog/**/*.md', sortBy: 'date', reverse: true}}},
         {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
         {name: 'wrap'},
@@ -75,19 +76,21 @@ module.exports = {
     middleware: function(source) {
       return [
         {name: 'asset', args: {src: 'images', dest: 'images'}},
-        {name: 'collections', args: {recipes: {pattern: 'recipes/**/*.md', sortBy: 'date', reverse: true}}},
+        {name: 'asset', args: {src: 'fonts', dest: 'fonts'}},
+        {name: 'collections', args: {posts: {pattern: 'blog/**/*.md', sortBy: 'date', reverse: true}}},
         {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
         {name: 'wrap'},
-        {name: 'clean', args: 'recipes'},
-        {name: 'page', args: {collection: 'recipes', perPage: 3, target: 'recipes', template: 'recipes'}},
+        {name: 'clean', args: 'blog'},
+        {name: 'page', args: {collection: 'posts', perPage: 7, target: 'blog', template: 'blog', perma: true}},
         {name: 'bind_template'},
+        {name: 'two_column'},
+        {name: 'row'},
         {name: 'partial'},
         {name: 'expose', args: {moment: moment}},
         {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
         {name: 'sass', args: {outputDir: 'assets/css'}},
         {name: 'uglify', args: {removeOriginal: true}},
         {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
-        {name: 'move', args: {source: '_code/fonts', destination: 'assets/fonts'}},
         {name: 'clean', args: '_code'},
         {name: 'clean', args: '.pony-token'}
       ];
