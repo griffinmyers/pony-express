afc791814d6f519457fd7b52bafb36ce0ca38733
Will Myers <griffin.myers@gmail.com>
2015-03-31 22:55:05 -0400

    Adding test for paging

--- test/lib/middleware.js
+++ test/lib/middleware.js
@@ -4,6 +4,7 @@ var bind_template = middleware.bind_template;
 var clean = middleware.clean;
 var expose = middleware.expose;
 var move = middleware.move;
+var page = middleware.page;
 
 describe('Middlware', function() {
 
@@ -96,5 +97,171 @@ describe('Middlware', function() {
     });
 
   });
+
+  describe('page()', function() {
+
+    var metalsmith;
+
+    beforeEach(function() {
+      metalsmith = {
+        '_metadata': {
+          'posts': [
+            {date: '2015-01-01', title: 'Post 1'},
+            {date: '2015-01-02', title: 'Post 2'},
+            {date: '2015-01-03', title: 'Post 3'},
+            {date: '2015-01-04', title: 'Post 4'}
+          ]
+        }
+      };
+    });
+
+    it('pages', function() {
+      page()(files, metalsmith);
+      files.should.have.property('blog/index.html');
+      files['blog/index.html'].should.have.property('prev', null);
+      files['blog/index.html'].should.have.property('next', 2);
+      files['blog/index.html'].should.have.property('target', 'blog');
+      files['blog/index.html'].should.have.property('template', 'page');
+      files['blog/index.html'].should.have.property('contents');
+      files['blog/index.html']['contents'].should.be.type('object');
+      files['blog/index.html'].should.have.property('posts');
+      files['blog/index.html']['posts'].should.have.length(3);
+
+      files.should.have.property('blog/2/index.html');
+      files['blog/2/index.html'].should.have.property('prev', 1);
+      files['blog/2/index.html'].should.have.property('next', null);
+      files['blog/2/index.html'].should.have.property('posts');
+      files['blog/2/index.html']['posts'].should.have.length(1);
+
+      files['blog/index.html'].posts[0].should.have.property('date', '2015-01-01');
+      files['blog/index.html'].posts[0].should.have.property('title', 'Post 1');
+      files['blog/index.html'].posts[0].should.have.property('date_string', '2015-01-01');
+
+      files['blog/index.html'].posts[1].should.have.property('date', '2015-01-02');
+      files['blog/index.html'].posts[1].should.have.property('title', 'Post 2');
+      files['blog/index.html'].posts[1].should.have.property('date_string', '2015-01-02');
+
+      files['blog/index.html'].posts[2].should.have.property('date', '2015-01-03');
+      files['blog/index.html'].posts[2].should.have.property('title', 'Post 3');
+      files['blog/index.html'].posts[2].should.have.property('date_string', '2015-01-03');
+
+      files['blog/2/index.html'].posts[0].should.have.property('date', '2015-01-04');
+      files['blog/2/index.html'].posts[0].should.have.property('title', 'Post 4');
+      files['blog/2/index.html'].posts[0].should.have.property('date_string', '2015-01-04');
+    });
+
+    it('pages the links properly', function() {
+      page({perPage: 1})(files, metalsmith);
+      files.should.have.property('blog/index.html');
+      files['blog/index.html'].should.have.property('prev', null);
+      files['blog/index.html'].should.have.property('next', 2);
+
+      files.should.have.property('blog/2/index.html');
+      files['blog/2/index.html'].should.have.property('prev', 1);
+      files['blog/2/index.html'].should.have.property('next', 3);
+
+      files.should.have.property('blog/3/index.html');
+      files['blog/3/index.html'].should.have.property('prev', 2);
+      files['blog/3/index.html'].should.have.property('next', 4);
+
+      files.should.have.property('blog/4/index.html');
+      files['blog/4/index.html'].should.have.property('prev', 3);
+      files['blog/4/index.html'].should.have.property('next', null);
+    });
+
+    it('lets me change collections', function() {
+      metalsmith._metadata['boops'] = metalsmith._metadata['posts'];
+      delete metalsmith._metadata['posts'];
+
+      page({collection: 'boops'})(files, metalsmith);
+      files.should.have.property('blog/index.html');
+      files.should.have.property('blog/2/index.html');
+    });
+
+    it('lets me change targets', function() {
+      page({target: 'boops'})(files, metalsmith);
+      files.should.have.property('boops/index.html');
+      files.should.have.property('boops/2/index.html');
+    });
+
+
+    it('lets me change templates', function() {
+      page({template: 'boops'})(files, metalsmith);
+      files.should.have.property('blog/index.html');
+      files['blog/index.html'].should.have.property('template', 'boops');
+      files.should.have.property('blog/2/index.html');
+      files['blog/2/index.html'].should.have.property('template', 'boops');
+    });
+
+    it('lets me create permalinks', function() {
+      page({perma: 'true'})(files, metalsmith);
+      files.should.have.property('blog/index.html');
+      files.should.have.property('blog/2/index.html');
+
+      files.should.have.property('blog/archive/a52dc9c71d83643d76dfd94d911475c3aa3e29b0/index.html');
+      files['blog/archive/a52dc9c71d83643d76dfd94d911475c3aa3e29b0/index.html'].should.have.property('template', 'page');
+      files['blog/archive/a52dc9c71d83643d76dfd94d911475c3aa3e29b0/index.html'].should.have.property('prev', null);
+      files['blog/archive/a52dc9c71d83643d76dfd94d911475c3aa3e29b0/index.html'].should.have.property('next', null);
+      files['blog/archive/a52dc9c71d83643d76dfd94d911475c3aa3e29b0/index.html'].should.have.property('contents')
+      files['blog/archive/a52dc9c71d83643d76dfd94d911475c3aa3e29b0/index.html']['contents'].should.be.type('object');
+
+      files.should.have.property('blog/archive/0e8a3466832ec5cebefd8cc299e240383be38177/index.html');
+      files['blog/archive/0e8a3466832ec5cebefd8cc299e240383be38177/index.html'].should.have.property('template', 'page');
+      files['blog/archive/0e8a3466832ec5cebefd8cc299e240383be38177/index.html'].should.have.property('prev', null);
+      files['blog/archive/0e8a3466832ec5cebefd8cc299e240383be38177/index.html'].should.have.property('next', null);
+      files['blog/archive/0e8a3466832ec5cebefd8cc299e240383be38177/index.html'].should.have.property('contents')
+      files['blog/archive/0e8a3466832ec5cebefd8cc299e240383be38177/index.html']['contents'].should.be.type('object');
+
+
+      files.should.have.property('blog/archive/90c5cd937ff6c221a14b89799c4c541ad6cf1e0a/index.html');
+      files['blog/archive/90c5cd937ff6c221a14b89799c4c541ad6cf1e0a/index.html'].should.have.property('template', 'page');
+      files['blog/archive/90c5cd937ff6c221a14b89799c4c541ad6cf1e0a/index.html'].should.have.property('prev', null);
+      files['blog/archive/90c5cd937ff6c221a14b89799c4c541ad6cf1e0a/index.html'].should.have.property('next', null);
+      files['blog/archive/90c5cd937ff6c221a14b89799c4c541ad6cf1e0a/index.html'].should.have.property('contents')
+      files['blog/archive/90c5cd937ff6c221a14b89799c4c541ad6cf1e0a/index.html']['contents'].should.be.type('object');
+
+
+      files.should.have.property('blog/archive/b515c81d47640fb1d19b6441e7902e37070b2dc6/index.html');
+      files['blog/archive/b515c81d47640fb1d19b6441e7902e37070b2dc6/index.html'].should.have.property('template', 'page');
+      files['blog/archive/b515c81d47640fb1d19b6441e7902e37070b2dc6/index.html'].should.have.property('prev', null);
+      files['blog/archive/b515c81d47640fb1d19b6441e7902e37070b2dc6/index.html'].should.have.property('next', null);
+      files['blog/archive/b515c81d47640fb1d19b6441e7902e37070b2dc6/index.html'].should.have.property('contents')
+      files['blog/archive/b515c81d47640fb1d19b6441e7902e37070b2dc6/index.html']['contents'].should.be.type('object');
+
+      files['blog/index.html']['posts'][0].should.have.property('perma_link', '/blog/archive/a52dc9c71d83643d76dfd94d911475c3aa3e29b0');
+      files['blog/index.html']['posts'][1].should.have.property('perma_link', '/blog/archive/0e8a3466832ec5cebefd8cc299e240383be38177');
+      files['blog/index.html']['posts'][2].should.have.property('perma_link', '/blog/archive/90c5cd937ff6c221a14b89799c4c541ad6cf1e0a');
+      files['blog/2/index.html']['posts'][0].should.have.property('perma_link', '/blog/archive/b515c81d47640fb1d19b6441e7902e37070b2dc6');
+    });
+
+    it('deals with parsed dates', function() {
+      metalsmith = {
+        '_metadata': {
+          'posts': [
+            {date: new Date('2015-01-01'), title: 'Post 1'},
+            {date: new Date('2015-01-02'), title: 'Post 2'},
+            {date: new Date('2015-01-03'), title: 'Post 3'},
+            {date: new Date('2015-01-04'), title: 'Post 4'}
+          ]
+        }
+      };
+
+      page()(files, metalsmith);
+
+      files['blog/index.html'].posts[0]['date'].should.be.type('object');
+      files['blog/index.html'].posts[0].should.have.property('date_string', '2015-01-01');
+
+      files['blog/index.html'].posts[1]['date'].should.be.type('object');
+      files['blog/index.html'].posts[1].should.have.property('date_string', '2015-01-02');
+
+      files['blog/index.html'].posts[2]['date'].should.be.type('object');
+      files['blog/index.html'].posts[2].should.have.property('date_string', '2015-01-03');
+
+      files['blog/2/index.html'].posts[0]['date'].should.be.type('object');
+      files['blog/2/index.html'].posts[0].should.have.property('date_string', '2015-01-04');
+
+    });
+
+ });
 
 });
