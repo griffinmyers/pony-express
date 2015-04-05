896caa1ce07d5e81f02145dce7f6e3deb626c862
Will Myers <griffin.myers@gmail.com>
2015-04-04 21:38:23 -0400

    Wrote some tests, found some bugs.

---
+++ test/lib/store.js
@@ -0,0 +1,91 @@
+var url = require('url');
+var should = require('should');
+var qs = require('querystring');
+var nock = require('nock');
+var Store = require('../../lib/store.js');
+
+// nock.recorder.rec();
+
+describe('Store', function() {
+
+  var store;
+
+  beforeEach(function() {
+    store = new Store('bucket', true);
+  });
+
+  describe('get', function() {
+
+    it('gets something', function(done) {
+
+      var amazon = nock('https://bucket.s3.amazonaws.com:443')
+        .get('/boop')
+        .reply(200, 'bleep');
+
+      store.get('boop').then(function(res) {
+        res.Body.toString().should.be.exactly('bleep');
+        amazon.done();
+        done();
+      }, done).done();
+
+    });
+
+    it('caches', function(done) {
+
+      var amazon = nock('https://bucket.s3.amazonaws.com:443')
+        .get('/boop')
+        .reply(200, 'bleep');
+
+      store.get('boop').then(function(res) {
+        res.Body.toString().should.be.exactly('bleep');
+        amazon.done();
+      }).then(function() {
+        return store.get('boop');
+      }).then(function(res) {
+        res.Body.toString().should.be.exactly('bleep');
+        done();
+      }, done).done();
+
+    });
+
+    it('can not cache', function(done) {
+
+    var uncached_store = new Store('bucket');
+    var amazon = nock('https://bucket.s3.amazonaws.com:443')
+      .get('/boop')
+      .times(2)
+      .reply(200, 'bleep');
+
+      uncached_store.get('boop').then(function(res) {
+        res.Body.toString().should.be.exactly('bleep');
+      }).then(function() {
+        return uncached_store.get('boop');
+      }).then(function(res) {
+        res.Body.toString().should.be.exactly('bleep');
+        amazon.done();
+        done()
+      }, done).done();
+
+    });
+
+  });
+
+  describe('put', function() {
+
+    it('puts something', function(done) {
+
+      var amazon = nock('https://bucket.s3.amazonaws.com:443')
+        .put('/boop', 'bleep')
+        .reply(200);
+
+      store.put('boop', 'bleep').then(function() {
+        amazon.done();
+        done();
+      }, done).done();
+
+    });
+
+  });
+
+});
+
