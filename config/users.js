e9b2ff22e03629cfc2c489cc54dd4977986a0b8b
Will Myers <griffin.myers@gmail.com>
2015-05-03 23:28:16 -0400

    Add shredski

--- config/users.js
+++ config/users.js
@@ -71,8 +71,8 @@ module.exports = {
       ];
     }
   },
-  dev: {
-    bucket: 'pony-express-dev',
+  103170856: {
+    bucket: 'ramblinrunner.com',
     middleware: function(source) {
       return [
         {name: 'asset', args: {src: 'images', dest: 'images'}},
@@ -81,7 +81,7 @@ module.exports = {
         {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
         {name: 'wrap'},
         {name: 'clean', args: 'blog'},
-        {name: 'page', args: {collection: 'posts', perPage: 7, target: 'blog', template: 'blog', perma: true}},
+        {name: 'page', args: {collection: 'posts', perPage: 3, target: '', template: 'blog', perma: true}},
         {name: 'bind_template'},
         {name: 'two_column'},
         {name: 'row'},
@@ -89,8 +89,6 @@ module.exports = {
         {name: 'expose', args: {moment: moment}},
         {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
         {name: 'sass', args: {outputDir: 'assets/css'}},
-        {name: 'uglify', args: {removeOriginal: true}},
-        {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
         {name: 'clean', args: '_code'},
         {name: 'clean', args: '.pony-token'}
       ];
