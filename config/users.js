d17016fda5bef2bc842ed9bb5bae9217b04060b5
Will Myers <griffin.myers@gmail.com>
2015-08-04 10:12:13 -0600

    Modify users.js

--- config/users.js
+++ config/users.js
@@ -94,7 +94,7 @@ module.exports = {
       ];
     }
   },
-  dev: {
+  'brandonkreiler': {
     bucket: 'brandonkreitler.com',
     middleware: function(source) {
       return [
@@ -103,6 +103,27 @@ module.exports = {
         {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
         {name: 'bind_template'},
         {name: 'partial'},
+        {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
+        {name: 'sass', args: {outputDir: 'assets/css'}},
+        {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
+        {name: 'clean', args: '_code'},
+        {name: 'clean', args: '.pony-token'}
+      ]
+    }
+  },
+  'cragcation': {
+    bucket: 'cragcation.com',
+    middleware: function(source) {
+      return [
+        {name: 'asset', args: {src: 'images', dest: 'images'}},
+        {name: 'collections', args: {posts: {pattern: 'posts/**/*.md', sortBy: 'date', reverse: true}}},
+        {name: 'markdown', args: {gfm: true, tables: true, breaks: false, pedantic: false, sanitize: false, smartLists: true, smartypants: true}},
+        {name: 'wrap'},
+        {name: 'clean', args: 'posts'},
+        {name: 'page', args: {collection: 'posts', perPage: 3, target: '', template: 'post', perma: true}},
+        {name: 'bind_template'},
+        {name: 'partial'},
+        {name: 'expose', args: {moment: moment}},
         {name: 'templates', args: {engine: 'jade', directory: path.join(source, '_code', 'templates')}},
         {name: 'sass', args: {outputDir: 'assets/css'}},
         {name: 'move', args: {source: '_code/scripts', destination: 'assets/scripts'}},
